6dfd6f7953a8e4d822dff70ae5f79d773da142ea
Noreddine Madi <adam.smit2020@gmail.com>
2023-02-26 21:06:41 -0500

    method return sql connection

--- Function.cs
+++ Function.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,11 @@ namespace Quiz
 {
     internal class Function
     {
+        protected SqlConnection getConnection()
+        {
+            SqlConnection con = new SqlConnection();
+            return con;
+        }
+
     }
 }
